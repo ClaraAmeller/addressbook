b015039543ede4aaecbf87afdb8ba6a0c1258a24
Clara Ameller <claraameller7@gmail.com>
2018-01-28 16:40:33 +0100

    Add validation to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :telephone, presence: true, length: { minimum: 9 }
+  validates :email, length: { minimum: 6 }
 end
